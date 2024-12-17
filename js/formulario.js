9efed54336585b38b0f46c910c69f1585df3fc80
juanchiiv <juanchiilobe@gmail.com>
2024-12-17 19:00:53 -0300

    Arreglo de acordeon

--- js/formulario.js
+++ js/formulario.js
@@ -4,23 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const cancelBtn = document.getElementById("cancelBtn");
   const content = document.getElementById("content");
 
-  //funcion para quitar el efecto borroso cuando se inicializa la pagina
-  function inicializar() {
-    //content.classList.remove("blurred-content");
-  }
-  //inicializar();
-
   // Mostrar el formulario al presionar el botón "Login"
   loginBtn.addEventListener("click", () => {
     overlay.style.display = "flex";
     content.classList.add("blurred-content");
   });
 
-  // Ocultar el formulario y quitar el efecto borroso al presionar "Cancelar"
-  cancelBtn.addEventListener("click", () => {
-    overlay.style.display = "none";
-    content.classList.remove("blurred-content");
-  });
+  // // Ocultar el formulario y quitar el efecto borroso al presionar "Cancelar"
+  // cancelBtn.addEventListener("click", () => {
+  //   overlay.style.display = "none";
+  //   content.classList.remove("blurred-content");
+  // });
 
   // También podrías agregar funcionalidad para enviar el formulario si lo deseas
   document
