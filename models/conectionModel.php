702f2a4795a4df9caddad5352e961d1d793bf330
juanchiiv <juanchiilobe@gmail.com>
2025-05-21 17:10:04 -0300

    fix

--- models/conectionModel.php
+++ models/conectionModel.php
@@ -1,29 +1,19 @@
 <?php
 
-class ConectionModel
-{
-
+class ConectionModel{
+    
     protected $db;
 
-    function __construct()
-    {
+    function __construct() {
         $user = 'root';
         $pass = '';
         $dbname = 'campamento_municipaldb'; /*cambiar nombre base de datos*/
         $host = 'localhost';
         $port = '3306';
-        try {
-            $this->db = new PDO('mysql:host=localhost;dbname=campamento_municipaldb;charset=utf8', 'root', '');
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            error_log('ERROR DB: ' . $e->getMessage());
-            die('No se pudo conectar a la base de datos.');
-        }
-
+        $this->db = new PDO("mysql:host=$host:$port;dbname=$dbname", $user, $pass);
     }
 
-    public function getConection()
-    {
+    public function getConection() {
         return $this->db;
     }
 }
